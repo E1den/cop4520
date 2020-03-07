eac4092c6c2f82f7e9bc8c930bafab7c6169831a
E1den <andrew.d.kiner@knights.ucf.edu>
2020-03-07 16:05:16 -0500

    working mrlock mcas

--- ProjectAssignment1/ProjectAssignment1/main.cc
+++ ProjectAssignment1/ProjectAssignment1/main.cc
@@ -1,5 +1,8 @@
 #include <iostream>
-#include "seq_mcas.h"
+#include <thread>
+#include "mrlock_mcas.h"
+
+MRLock<uint64_t>* mrlock_mcas::m = nullptr;
 
 int main(int argc, char** argv)
 {
@@ -8,26 +11,41 @@ int main(int argc, char** argv)
 	long three = 3;
 
 	casRow arr[3];
-	
+
 	arr[0] = {};
 	arr[0].address = &one;
 	arr[0].expectedValue = 1;
 	arr[0].newValue = 37;
+	arr[0].resId = 0b1;
 
 	arr[1] = {};
 	arr[1].address = &two;
 	arr[1].expectedValue = 2;
 	arr[1].newValue = 38;
+	arr[0].resId = 0b10;
 
 	arr[2] = {};
 	arr[2].address = &three;
 	arr[2].expectedValue = 3;
 	arr[2].newValue = 39;
+	arr[0].resId = 0b100;
 
+	mrlock_mcas::intial(sizeof(arr) / sizeof(casRow));
 
-	std::cout << "Before: " << one << ", " << two << ", " << three << std::endl;
-	seq_mcas::invokeMcas(arr, &arr[2]);
-	std::cout << "After: " << one << ", " << two << ", " << three << std::endl;
+	const uint32_t numThreads = 4;
+
+	std::thread threads[numThreads];
+
+	for (auto i = 0; i < numThreads; i++)
+		threads[i] = std::thread([&] {
+		for (;;) {
+			std::cout << "Before: " << one << ", " << two << ", " << three << std::endl;
+			mrlock_mcas::invokeMcas(arr, &arr[2]);
+			std::cout << "After: " << one << ", " << two << ", " << three << std::endl;
+		}});
+
+	for (auto i = 0; i < numThreads; i++)
+		threads[i].join();
 
 	return 0;
 }
--- ProjectAssignment1/ProjectAssignment1/mrlock_mcas.h
+++ ProjectAssignment1/ProjectAssignment1/mrlock_mcas.h
@@ -1,39 +1,51 @@
 #pragma once
 #include <atomic>
 #include <memory>
+#include <random>
 #include "mrlock.h"
 
 struct casRow {
 	long *address;
 	long expectedValue;
 	long newValue;
+	uint32_t resId;
 	uint32_t handle;
 };
 
-class seq_mcas
+class mrlock_mcas
 {
 public:
-	inline static void complete(casRow* start, casRow* last, MRLock<casRow*>& m)
+	inline static void intial(uint32_t res)
+	{
+		m = new MRLock<uint64_t>(res*2);
+	}
+
+	inline static void complete(casRow* start, casRow* last,bool success=true)
 	{
 		for (casRow* runner = start; runner <= last; runner++)
 		{
-			(*runner->address) = runner->newValue;
-			m.Unlock(runner->handle);
+			if(success)
+				(*runner->address) = runner->newValue;
+			m->Unlock(runner->handle);
 		}
 	}
 
 	inline static void invokeMcas(casRow* start, casRow* last)
 	{
-		MRLock<casRow*> m = MRLock<casRow*>(0x1);
-
-
-		for (casRow* runner = start; runner<=last; runner ++)
+		
+		for (casRow* runner = start; runner <= last; runner++)
 		{
-			runner->handle = m.Lock(runner);
+			runner->handle = m->Lock(runner->resId);
 			if (*runner->address != runner->expectedValue)
+			{
+				complete(start, runner, false);
 				return;
+			}
 		}
 		//at this point all are equal, modify them to the new values
-		complete(start, last,m);
+		complete(start, last);
 	}
+
+private:
+	static MRLock<uint64_t>* m;
 };
