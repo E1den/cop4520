bdf68904019f06098f33069d7adc968050d3bd48
E1den <andrew.d.kiner@knights.ucf.edu>
2020-03-22 17:21:50 -0400

    Step 4

--- ProjectAssignment1/ProjectAssignment1/main.cc
+++ ProjectAssignment1/ProjectAssignment1/main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <thread>
+#include <ctime>
 #include "mrlock_mcas.h"
 
 MRLock<uint64_t>* mrlock_mcas::m = nullptr;
@@ -30,18 +31,65 @@ int main(int argc, char** argv)
 	arr[2].newValue = 39;
 	arr[0].resId = 0b100;
 
+	casRow arr2[3];
+
+	arr2[0] = {};
+	arr2[0].address = &one;
+	arr2[0].expectedValue = 37;
+	arr2[0].newValue = 5;
+	arr2[0].resId = 0b1;
+
+	arr2[1] = {};
+	arr2[1].address = &two;
+	arr2[1].expectedValue = 38;
+	arr2[1].newValue = 6;
+	arr2[0].resId = 0b10;
+
+	arr2[2] = {};
+	arr2[2].address = &three;
+	arr2[2].expectedValue = 39;
+	arr2[2].newValue = 7;
+	arr2[0].resId = 0b100;
+
+
+	casRow arr3[3];
+
+	arr3[0] = {};
+	arr3[0].address = &one;
+	arr3[0].expectedValue = 5;
+	arr3[0].newValue = 1;
+	arr3[0].resId = 0b1;
+
+	arr3[1] = {};
+	arr3[1].address = &two;
+	arr3[1].expectedValue = 6;
+	arr3[1].newValue = 2;
+	arr3[0].resId = 0b10;
+
+	arr3[2] = {};
+	arr3[2].address = &three;
+	arr3[2].expectedValue = 7;
+	arr3[2].newValue = 3;
+	arr3[0].resId = 0b100;
+
 	mrlock_mcas::intial(sizeof(arr) / sizeof(casRow));
 
 	const uint32_t numThreads = 4;
 
 	std::thread threads[numThreads];
 
+	std::srand(std::time(nullptr));
+
 	for (auto i = 0; i < numThreads; i++)
 		threads[i] = std::thread([&] {
-		for (;;) {
-			std::cout << "Before: " << one << ", " << two << ", " << three << std::endl;
-			mrlock_mcas::invokeMcas(arr, &arr[2]);
-			std::cout << "After: " << one << ", " << two << ", " << three << std::endl;
+		for (int x=0;x<10;x++) {
+			int i = rand() % 3;
+			if(i==0)
+				mrlock_mcas::invokeMcas(arr, &arr[2]);
+			else if (i == 1)
+				mrlock_mcas::invokeMcas(arr2, &arr2[2]);
+			else if (i == 2)
+				mrlock_mcas::invokeMcas(arr3, &arr3[2]);
 		}});
 
 	for (auto i = 0; i < numThreads; i++)
